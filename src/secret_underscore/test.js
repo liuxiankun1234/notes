7c61999e54e6893600920d22dfc6878a19d09d7d
liuxiankun <liuxiankun@zhangyue.com>
2020-01-19 14:04:07 +0800

    _.each方法完结

--- src/secret_underscore/test.js
+++ src/secret_underscore/test.js
@@ -1,4 +1,28 @@
 import _ from './underscore.js';
 
-console.log(
-)
+
+void function() {
+    /**
+     *  _.each
+     *  类型处理
+     *      Number      不处理
+     *      Boolean     不处理
+     *      null        不处理
+     *      undefined   不处理
+     *      function    不处理
+     *      正则         不处理     
+     * 
+     *      String      迭代处理
+     *      Array       迭代处理
+     *      Object      迭代处理
+     *      
+    **/
+    _.each([1,2,3], function(...arg) {
+        console.log(...arg, '---------')
+    })
+    _.each({a: 1, b: 2}, console.log, {})
+
+    _.each(/12/, function() {
+        console.log('sdfasdkfasdjfksajfdk')
+    })
+}();
--- src/talk_about/index.js
+++ src/talk_about/index.js
@@ -1,8 +1,16 @@
+/**
+ *  鲜为人知的事实：Array.prototype 是一个数组类型
+ * 
+ * 
+ * 
+ * 
+**/
+
 // import './talk_about_严格模式'
 // import './talk_about_包装类'
 // import './talk_about_获取对象属性'
 // import './talk_about_正则表达式'
 // import './talk_about_运算符优先级';
 // import './talk_about_promise'
-import './talk_about_ generator'
-// 鲜为人知的事实：Array.prototype 是一个数组类型
+// import './talk_about_ generator'
+import './talk_about_数组方法'
