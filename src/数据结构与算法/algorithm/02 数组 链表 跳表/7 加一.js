1b38cea57c7fc8e467bffac89bfbbcce93431156
liuxiankun <18246059391@163.com>
2021-03-26 00:11:52 +0800

    feat:数组简单题完成

---
+++ src/数据结构与算法/algorithm/02 数组 链表 跳表/7 加一.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ *  题目
+ *      加一
+ *  地址
+ *      https://leetcode-cn.com/problems/plus-one/
+ *  
+ *  解题思路
+ *      让尾部数字加1，如果不等于0 那么直接return出去
+ *      如果等于0 则继续前一位加一 循环
+ *      遍历完 还没返回则处理前置加一 [9] [9,9] 这种情况
+ *          
+*/
+
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+    var len = digits.length;
+
+    for(var i = len - 1; i >= 0; i--) {
+        digits[i] = ++digits[i] % 10
+        if(digits[i] !== 0) {
+            return digits
+        }
+    }
+    return [1, ...digits]
+};
