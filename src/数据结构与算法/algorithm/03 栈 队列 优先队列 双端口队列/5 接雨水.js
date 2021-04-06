4e674cd7972c1957cb1f0c55a14f1bafe1a77fb1
liuxiankun <18246059391@163.com>
2021-04-07 00:20:29 +0800

    feat:栈

--- src/数据结构与算法/algorithm/03 栈 队列 优先队列 双端口队列/5 接雨水.js
+++ src/数据结构与算法/algorithm/03 栈 队列 优先队列 双端口队列/5 接雨水.js
@@ -6,25 +6,32 @@
  *  解题思路
  *      1、
 */
-
 /**
  * @param {number[]} height
  * @return {number}
  */
 var trap = function(height) {
+    debugger
     var len = height.length,
     area = 0,
-    stack  =[];
+    stack = [];
 
     for(var i = 0; i < len; i++) {
         while(stack.length && height[i] > height[stack[stack.length - 1]]) {
             const top = stack.pop();
-            if (!stack.length) {
+
+            if(stack.length === 0) {
                 break;
             }
-            
+
+            const left = stack[stack.length - 1]
+            const currWidth = i - left - 1;
+
+            const currHeight = Math.min(height[left], height[i]) - height[top];
+            area += currWidth * currHeight;
         }
         stack.push(i)
     }
     return area;
 };
+trap([0,1,0,2,1,0,1,3,2,1,2,1])
