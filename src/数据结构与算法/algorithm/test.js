835d4fa559b90ed1ebcf65613b255b30e32be650
liuxiankun <18246059391@163.com>
2021-04-18 23:03:44 +0800

    feat:浏览器学习

--- src/数据结构与算法/algorithm/test.js
+++ src/数据结构与算法/algorithm/test.js
@@ -1,28 +1,82 @@
-var maxSlidingWindow = function(nums, k) {
-    const n = nums.length;
-    const prefixMax = new Array(n).fill(0);
-    const suffixMax = new Array(n).fill(0);
-    for (let i = 0; i < n; i++) {
-        if (i % k === 0) {
-            prefixMax[i] = nums[i];
-        } else {
-            prefixMax[i] = Math.max(prefixMax[i - 1], nums[i]);
-        }
-    }
-    for (let i = n - 1; i >= 0; --i) {
-        if (i === n || (i + 1) % k === 0) {
-            suffixMax[i] = nums[i];
-        } else {
-            suffixMax[i] = Math.max(suffixMax[i + 1], nums[i]);
-        }
-    }
-    const ans = [];
-    for (let i = 0; i < n - k + 1; i++) {
-        ans.push(Math.max(suffixMax[i], prefixMax[i + k - 1]));
-    }
-    return ans;
-};  
+/**
+ *  理解URL到bitMap过程
+ *      URL-(HTTP)->HTML-(parse)->DOM-(css computing)-> DOM with CSS -(layout)->DOM with position -(render)->bitMap
+ *  有限状态机
+ *      特点
+ *          每一个状态都是一个机器
+ *              在每一个状态里我们都可以计算、存储、输出
+ *              所有状态接收的输入都是一致的
+ *              状态机的每一个机器本身没有状态，我们用函数表示的话，他应该是一个纯函数
+ *          每个机器都知道下一个状态
+ *              每个机器都有确定的下一个状态（Moore状态机）
+ *              每个机器根据输入决定下一个状态（Mealy状态机）
+ *  Moore状态机
+ *  Mealy状态机
+ *  有限状态机实现正则？？？
+ *  https://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm???
+ * 
+*/
 
-nums = [1,3,-1,-3,5,3,6,7]
-k = 3
-maxSlidingWindow(nums, k)
+/**
+ * 状态机实现字符串搜索
+*/
+function match(string) {
+    let state = start;
+    for(let c of string) {
+        state = state(c)
+    }
+    return state === end
+}
+
+function end(c) {
+    return end
+}
+
+function start(c) {
+    if(c === 'a') {
+        return foundA
+    }
+    return start;
+}
+
+function foundA(c) {
+    if(c === 'b') {
+        return foundB;
+    }
+    return start(c)
+}
+function foundB(c) {
+    if(c === 'a') {
+        return foundA2;
+    }
+    return start(c)
+}
+function foundA2(c) {
+    if(c === 'b') {
+        return foundB2;
+    }
+    return start(c)
+}
+
+function foundB2(c) {
+    if(c === 'a') {
+        return foundA3;
+    }
+    return start(c)
+}
+function foundA3(c) {
+    if(c === 'b') {
+        return foundB3;
+    }
+    return start(c)
+}
+function foundB3(c) {
+    if(c === 'x') {
+        return end;
+    }
+    if(c === 'a') {
+        return foundA3;
+    }
+    return start(c)
+}
+match('ababababx')
