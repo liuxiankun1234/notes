8347494ec8427a3eb98e11f53ccef3ea2d145cf6
liuxiankun <18246059391@163.com>
2021-07-11 09:52:09 +0800

    feat: partial函数

--- src/源码相关/secret_underscore/new_underscore.js
+++ src/源码相关/secret_underscore/new_underscore.js
@@ -938,6 +938,37 @@
         })
        return bound;
    })
+
+   // Partially apply a function by creating a version that has had some of its
+    // arguments pre-filled, without changing its dynamic `this` context. _ acts
+    // as a placeholder by default, allowing any combination of arguments to be
+    // pre-filled. Set `_.partial.placeholder` for a custom placeholder argument.
+    _.partial = restArguments(function(func, boundArgs) {
+        var placeholder = _.partial.placeholder;
+        var bound = function() {
+            var position = 0;
+            var length = boundArgs.length
+
+            var args = Array(length);
+            for (var i = 0; i < length; i++) {
+                args[i] = 
+                    boundArgs[i] === placeholder
+                        ? arguments[position++]
+                        : boundArgs[i]
+            } 
+            while(position < arguments.length) args.push(arguments[position++])
+            return executeBound(
+                func,
+                bound,
+                this,
+                this,
+                args
+            )
+        }
+        return bound;
+    })
+    _.partial.placeholder = _
+
    // Bind a number of an object's methods to that object. Remaining arguments
     // are the method names to be bound. Useful for ensuring that all callbacks
     // defined on an object belong to it.
@@ -971,6 +1002,10 @@
             func.apply(null, rest)
         }, wait)
     })
+
+    // Defers a function, scheduling it to run after the current call stack has
+    // cleared.
+    _.defer = _.partial(_.delay, _, 1);
 
     // Returns a function, that, when invoked, will only be triggered at most once
     // during a given window of time. Normally, the throttled function will run
